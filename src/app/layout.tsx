3052b6f9d5af3fbd05246b9dc6c0e392b3a82343
ibnu choldun <ibnucholdun023@gmail.com>
2023-09-27 13:26:28 +0700

    Link and Navigation with useRouter()
    and implementation disable navbar with usePathname()

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./navbar";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
+const disableNavbar = ["/login", "/register"];
 
 export default function RootLayout({
   children,
@@ -14,10 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const [state, setState] = useState(0);
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
+        {!disableNavbar.includes(pathname) && <Navbar />}
         {/* <h1>layout : {state}</h1>
         <button onClick={() => setState(state + 1)}>Klik</button> */}
         {children}
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,43 +1,55 @@
 import Link from "next/link";
 import React from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   const pathname = usePathname();
+  const router = useRouter();
   return (
-    <nav className="flex bg-gray-800 py-2 px-5 justify-between">
-      <h1 className="text-white">Navbar</h1>
-      <ul className="flex ml-5">
-        <Link href="/">
-          <li
-            className={`mr-3 ${
-              pathname === "/" ? "text-blue-300" : "text-white"
-            } cursor-pointer`}
-          >
-            Home
-          </li>
-        </Link>
-        <Link href="/about">
-          <li
-            className={`mr-3 ${
-              pathname === "/about" ? "text-blue-300" : "text-white"
-            } cursor-pointer`}
-          >
-            About
-          </li>
-        </Link>
-        <Link href="/about/profile">
-          <li
-            className={`mr-3 ${
-              pathname === "/about/profile" ? "text-blue-300" : "text-white"
-            } cursor-pointer`}
-          >
-            Profile
-          </li>
-        </Link>
-      </ul>
+    <nav className="flex bg-gray-800 py-2 px-5 justify-between items-center">
+      <div className="flex">
+        <h1 className="text-white">Navbar</h1>
+        <ul className="flex ml-5">
+          <Link href="/">
+            <li
+              className={`mr-3 ${
+                pathname === "/" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li
+              className={`mr-3 ${
+                pathname === "/about" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              About
+            </li>
+          </Link>
+          <Link href="/about/profile">
+            <li
+              className={`mr-3 ${
+                pathname === "/about/profile" ? "text-blue-300" : "text-white"
+              } cursor-pointer`}
+            >
+              Profile
+            </li>
+          </Link>
+        </ul>
+      </div>
+
+      <div>
+        <button
+          className="text-white bg-blue-700 px-5 py-2 rounded-md cursor-pointer"
+          onClick={() => router.push("/login")}
+        >
+          Login
+        </button>
+      </div>
     </nav>
   );
 };
