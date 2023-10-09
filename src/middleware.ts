aa9d691c72168164db08cb36fec6323ee2eb982d
ibnu choldun <ibnucholdun023@gmail.com>
2023-10-09 15:27:17 +0700

    middleware with many pathname

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/about")) {
-    return NextResponse.redirect(new URL("/", request.url));
+  const isLogin = true;
+
+  if (!isLogin) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 }
+
+export const config = {
+  matcher: ["/product", "/dashboard/:path*"],
+};
