e84cc22f44233108b28f5884b5dddb6ae9aaf47a
ibnucholdun <ibnucholdun023@gmail.com>
2023-11-21 15:46:10 +0700

    add custom page error server 500

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-9xl font-extrabold">500</h1>
+      <p className="mb-5 text-3xl">Something went wrong!</p>
+    </div>
+  );
+};
+
+export default Error;
